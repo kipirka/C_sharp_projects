74f63b8e6ae04512c873012e66890ca33bdbe808
kipirka <kipirka@gmail.com>
2023-01-31 00:08:22 +0300

    Создаёт новую задачу.

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding= Encoding.Unicode;
+
+//Используйте все отношения к числам и выведите значение true
+
+int x = 2;
+int y = 4;
+
+bool z = (x + y > x - y) || (x * y < x / y);
+Console.WriteLine(z);
