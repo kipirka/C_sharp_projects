82ed884bf656b28e24a03127a3f5b7ab4a31e831
kipirka <kipirka@gmail.com>
2023-01-31 00:03:18 +0300

    writes an excercise

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text;
+Console.OutputEncoding = Encoding.Unicode;
+Console.InputEncoding = Encoding.Unicode;
+
+Console.WriteLine("Введите год");
+int year = Convert.ToInt16(Console.ReadLine());
+
+if ((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
+{
+    Console.WriteLine("год високосный");
+}
+   
+else
+{
+    Console.WriteLine("год невисокосный");
+}
+
