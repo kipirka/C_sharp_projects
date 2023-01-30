82bae422c1da64291de01bee6ee682d155c478d7
kipirka <kipirka@gmail.com>
2023-01-30 23:51:10 +0300

    Enters second problem

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -3,6 +3,11 @@ using System.Text;
 Console.OutputEncoding = Encoding.Unicode;
 Console.InputEncoding = Encoding.Unicode;
 
-bool a = true;
-bool b = false;
-Console.WriteLine((a && b) || (!a && !b));
+//bool a = true;
+//bool b = false;
+//Console.WriteLine((a && b) || (!a && !b));
+
+int x = 5;
+int y = 10;
+bool z = (y > Math.Pow(x,2) || y >= 2*x && x < y);
+Console.WriteLine(z);
